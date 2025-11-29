9427d17509a2d9c0240aaaec03b6f714f05fefda
Shaun Hossain <107396795+shaun4727@users.noreply.github.com>
2025-11-29 20:43:40 +0600

    updated

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ let net = require('net');
 let count = 0, users={};
 
 let server = net.createServer((conn) => {
-    conn.write('welcome to node chat\n '+ count + ' other people are connected at this time.\n' + 'Please write your name and press enter');
+    conn.write('welcome to node chat\n '+ count + ' other people are connected at this time.\n' + 'Please write your name and press enter: ');
     count++;
 
     conn.setEncoding('utf8');
 
+    const broadcast = (msg, exceptMyself) => {
+        for(var i in users){
+            if(!exceptMyself || i != nickname){
+                users[i].write(msg);
+            }
+        }
+    }
+
+        // broadcast(`${nickname} joined the room`);
+        // broadcast(`${nickname} ${data}`,true );
+
     let nickname;
     conn.on('data', (data) => {
+        let justJoined = false;
         data = data.replace('\r\n','');
         
         if(!nickname){
@@ -20,22 +32,35 @@ let server = net.createServer((conn) => {
                 nickname = data;
                 users[nickname] = conn;
 
-                for(var i in users){
-                    if(i != nickname){
+                justJoined = true;
+
+                broadcast(`${nickname} joined the room\n`);
+
+
+                // for(var i in users){
+                //     if(i != nickname){
                         // this condition ensures that message will be broadcasted to all other connected users 
 
-                        users[i].write(`\n ${nickname}${data} \n`);
-                    }
+                        // users[i].write(`\n ${nickname} joined the room \n`);
+                //     }
 
-                }
+                // }
             }
+        }
+
+        if(!justJoined){
+            broadcast(`${nickname}> ${data}\n`,true);
         }
     });
 
 
 
+
+
     conn.on('close', ()=>{
         count--;
+        broadcast(`${nickname} left the room\n`);
+        delete users[nickname];
     })
 })
 
